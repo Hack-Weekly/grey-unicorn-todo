fa95bb7c1c3b96f8b18008090a315d9cd81dc36a
Z-Index-1 <trawissomw@proton.me>
2023-03-08 18:19:36 -0400

    Added TodoItem and TodoItem Container components

---
+++ grey-unicorn-todo/src/components/TodoItem.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const TodoItem = () => {
+    return (
+        <div>
+						TodoItem
+        </div>
+    )
+}
+
+export default TodoItem;
---
+++ grey-unicorn-todo/src/components/TodoItemContainer.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+import TodoItem from "./TodoItem"
+
+const TodoItemContainer = () => {
+    return (
+        <div>
+						Todo Title
+						<TodoItem />
+        </div>
+    )
+}
+
+export default TodoItemContainer;
