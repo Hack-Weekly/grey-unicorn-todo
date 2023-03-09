633b08933246f784394db94466d1101257884632
harrybarcia12 <barcia.harry@gmail.com>
2023-03-09 11:28:15 -0800

    adding and deleting todos

--- grey-unicorn-todo/src/components/TodoItem.tsx
+++ grey-unicorn-todo/src/components/TodoItem.tsx
@@ -1,10 +1,42 @@
 
 import React from "react";
+import { useState } from "react";
 
 const TodoItem = () => {
+
+    const [list, setList] = useState([])
+    const [input, setInput] = useState('')
+  
+    const addTodo = (todo) => {
+      const newTodo = {
+        id: Math.random(),
+        todo: todo,
+      }
+      // add new todo to the list
+      setList([...list, newTodo])
+      // clear input
+      setInput('')
+    }
+  
+    const deleteTodo = (id) => {
+      // filter out the todo that has the id
+      setList(list.filter((todo) => todo.id !== id))
+    }
+
     return (
         <div>
-						TodoItem
+            <input type="text" value= {input} onChange={e => setInput(e.target.value)}/>
+            <button onClick={() => addTodo(input)}> Add </button>
+            <ul>
+                {list.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.todo}
+                        <button
+                            onClick={() => deleteTodo(todo.id)}
+                        > &times;</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
