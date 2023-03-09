97f1a98ca0cfae70f3f9d3d2018c93c88986df7c
Kari <kouglas2@gmail.com>
2023-03-09 16:15:19 -0600

    update TodoItem.tsx
    add ability to get list items from localStorage.
    add ability to set list items from localStorage

--- grey-unicorn-todo/src/components/TodoItem.tsx
+++ grey-unicorn-todo/src/components/TodoItem.tsx
@@ -1,11 +1,16 @@
 
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const TodoItem = () => {
 
-    const [list, setList] = useState([])
+    const [list, setList] = useState(
+        // initialize state by accessing localStorage 
+        // get item with key of 'list'
+        // parse value we're grabbing
+      JSON.parse(localStorage.getItem('list')) 
+      || [])
     const [input, setInput] = useState('')
   
     const addTodo = (todo) => {
@@ -17,7 +22,15 @@ const TodoItem = () => {
       setList([...list, newTodo])
       // clear input
       setInput('')
+    
     }
+
+        //interact with localStorage every time the list array changes
+    useEffect(() => {
+        //update the 'list' key inside localStorage
+        localStorage.setItem('list', JSON.stringify(list))
+    }, [list])
+
   
     const deleteTodo = (id) => {
       // filter out the todo that has the id
@@ -26,6 +39,7 @@ const TodoItem = () => {
 
     return (
         <div>
+          <form action=""></form>
             <input type="text" value= {input} onChange={e => setInput(e.target.value)}/>
             <button onClick={() => addTodo(input)}> Add </button>
             <ul>
